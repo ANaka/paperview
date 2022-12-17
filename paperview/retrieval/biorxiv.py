3535902e5ee61a7b87294ccbad206cc5121e2cf5
anaka <alex.naka@gmail.com>
2022-12-16 22:33:56 -0800

    eh lets just do functions

--- paperview/retrieval/biorxiv.py
+++ paperview/retrieval/biorxiv.py
@@ -1,14 +1,15 @@
 import requests
 from attrs import define, field
-
+import re
+from typing import List
 @define
 class Message:
     status: str = field()
-    interval: str = field()
-    cursor: str = field()
-    count: int = field()
-    count_new_papers: int = field()
-    total: int = field()
+    interval: str = field(default=None)
+    cursor: str = field(default=None)
+    count: int = field(default=None)
+    count_new_papers: int = field(default=None)
+    total: int = field(default=None)
     
     def __repr__(self):
         return (f"Message(status='{self.status}', interval='{self.interval}', "
@@ -20,7 +21,7 @@ def split_authors(authors: str) -> list:
     return authors.split('; ')
 
 @define
-class ManuscriptMetadata:
+class ArticleDetail:
     title: str = field()
     authors: list = field(converter=split_authors)
     date: str = field()
@@ -37,7 +38,7 @@ class ManuscriptMetadata:
     jatsxml: str = field()
     
     def __repr__(self):
-        return f"""ManuscriptMetadata(
+        return f"""ArticleDetail(
     title='{self.title}',
     authors={self.authors},
     date='{self.date}',
@@ -53,45 +54,76 @@ class ManuscriptMetadata:
     server='{self.server}',
     jatsxml='{self.jatsxml}')"""
     
+BASE_URL = "https://api.biorxiv.org"
 
-class BioRxivAPI:
-    def __init__(self, server=None, interval=None, cursor=0, format="json"):
-        self.base_url = "https://api.biorxiv.org/details/"
-        self.server = server
-        self.interval = interval
-        self.cursor = cursor
-        self.format = format
-       
-    def query(self):
-        if self.interval is not None:
-            # Construct the URL for a range of dates or most recent N posts or N days
-            url = f"{self.base_url}{self.server}/{self.interval}/{self.cursor}/{self.format}"
-        else:
-            # Construct the URL for a single manuscript using a DOI
-            url = f"{self.base_url}{self.server}/na/{self.format}"
-        
-        # Make the request
-        response = requests.get(url)
-        
-        # Return the response
-        return response
+def query_content_detail_by_doi(
+    doi:str, 
+    server:str='biorxiv', # biorxiv or medRxiv
+    format:str='JSON', # JSON or XML
+    ):
+    """https://api.biorxiv.org/details/[server]/[DOI]/na/[format] returns detail for a single manuscript. 
+    For instance, https://api.biorxiv.org/details/biorxiv/10.1101/339747 will output metadata for the biorxiv paper with DOI 10.1101/339747."""
+    url = f"{BASE_URL}/details/{server}/{doi}/na/{format}"
+    response = requests.get(url)
+    return response
+
+def validate_interval(interval):
+    # Check if the interval is a range of dates
+    if re.match(r'\d{4}-\d{2}-\d{2}/\d{4}-\d{2}-\d{2}', interval):
+        return True
     
-    def get_metadata(self):
-        # Get the response
-        response = self.query()
-        
-        # Parse the messages output
-        messages = response.json()['messages']
-        parsed_messages = [Message(**message) for message in messages]
-        
-        # Parse the collections output
-        collections = response.json()['collection']
-        parsed_collections = [ManuscriptMetadata(**collection) for collection in collections]
-        
-        # Return the parsed messages and collections
-        return parsed_messages, parsed_collections
+    # Check if the interval is a numeric value for the N most recent posts
+    if re.match(r'^\d+$', interval):
+        return True
     
-    def query_many_records(self, num_records):
-        # Initialize the cursor
-        cursor = 0
-        
+    # Check if the interval is a numeric value with the letter 'd' for the most recent N days of posts
+    if re.match(r'^\d+d$', interval):
+        return True
+    
+    return False
+
+def query_content_detail_by_interval(
+    interval: str,
+    cursor: int = 0,
+    server: str='biorxiv', # biorxiv or medRxiv
+    format: str='JSON', # JSON or XML
+    ):
+    """The format of the endpoint is https://api.biorxiv.org/details/[server]/[interval]/[cursor]/[format] or https://api.biorxiv.org/details/[server]/[DOI]/na/[format]
+    where 'interval' can be 1) two YYYY-MM-DD dates separted by '/' and 'cursor' is the start point which defaults to 0 if not supplied, or 2) a numeric value for the N most recent posts, or 3) a numeric with the letter 'd' for the most recent N days of posts.
+    Where metadata for multiple papers is returned, results are paginated with 100 papers served in a call. The 'cursor' value can be used to iterate through the result.
+    For instance, https://api.biorxiv.org/details/biorxiv/2018-08-21/2018-08-28/45 will output 100 results (if that many remain) within the date range of 2018-08-21 to 2018-08-28 beginning from result 45 for biorxiv."""
+    if not validate_interval(interval):
+        raise ValueError(f"Invalid interval: {interval}")
+    url = f"{BASE_URL}/details/{server}/{interval}/{cursor}/{format}"
+    response = requests.get(url)
+    # Parse the messages output
+    messages = response.json()['messages']
+    parsed_messages = [Message(**message) for message in messages]
+    
+    # Parse the collections output
+    collections = response.json()['collection']
+    parsed_collections = [ArticleDetail(**collection) for collection in collections]
+    
+    # Return the parsed messages and collections
+    return {'messages': parsed_messages, 'collections': parsed_collections}
+
+def get_all_content_details_by_interval(interval: str, server: str='bioRxiv', format: str='json') -> List[ArticleDetail]:
+    """Retrieve all pages of results for the given interval."""
+    all_results = []
+    cursor = 0
+    while True:
+        results = query_content_detail_by_interval(interval, cursor=cursor, server=server, format=format)
+        collections = results['collections']
+        all_results.extend(collections)
+        if len(collections) < 100:
+            # There are no more pages of results
+            break
+        cursor += 100
+    return all_results
+
+def query_article_by_doi(doi: str, server: str='biorxiv', format: str='JSON'):
+    """https://api.biorxiv.org/pubs/[server]/[DOI]/na/[format] returns detail for a single manuscript.
+    For instance, https://api.biorxiv.org/pubs/medrxiv/10.1101/2021.04.29.21256344 will output publication metadata for the biorxiv paper with DOI 10.1101/2021.04.29.21256344. Conversely, https://api.biorxiv.org/pubs/medrxiv/10.1371/journal.pone.0256482 will output publication metadata for the medRxiv paper with published DOI 10.1371/journal.pone.0256482.url = f"{BASE_URL}/details/{server}/{doi}/na/{format}"""
+    url = f"{BASE_URL}/pubs/{server}/{doi}/na/{format}"
+    response = requests.get(url)
+    return response
--- tests/retrieval/test_biorxiv.py
+++ tests/retrieval/test_biorxiv.py
@@ -1,41 +1,66 @@
 import pytest
 import requests
-from paperview.retrieval.biorxiv import BioRxivAPI, ManuscriptMetadata
+
+from paperview.retrieval.biorxiv import (
+    Message, 
+    ArticleDetail,
+    query_content_detail_by_doi,
+    query_content_detail_by_interval,
+    get_all_content_details_by_interval,
+    )
+
 
 @pytest.fixture
-def api():
-    return BioRxivAPI()
-
-def test_query(api):
-    # Test a query for a single manuscript using a DOI
-    api.server = "10.1101/2020.12.31.422412"
-    response = api.query()
-    assert response.status_code == 200
+def example_article_detail():
+    return {'doi': '10.1101/456574',
+    'title': 'Complementary subnetworks of cortical somatostatin interneurons',
+    'authors': 'Naka, A.; Shababo, B.; Snyder, B.; Egladyous, A.; Sridharan, S.; Paninski, L.; Adesnik, H.',
+    'author_corresponding': 'Hillel  Adesnik',
+    'author_corresponding_institution': 'UC Berkeley',
+    'date': '2018-10-30',
+    'version': '1',
+    'type': 'new results',
+    'license': 'cc_by_nc',
+    'category': 'neuroscience',
+    'jatsxml': 'https://www.biorxiv.org/content/early/2018/10/30/456574.source.xml',
+    'abstract': 'The neocortex is organized into discrete layers of excitatory neurons: layer 4 receives the densest  bottom up projection carrying external sensory data, while layers 2/3 and 5 receive  top down inputs from higher cortical areas that may convey sensory expectations and behavioral goals. A subset of cortical somatostatin (SST) neurons gate top down input and control sensory computation by inhibiting the apical dendrites of pyramidal cells in layers 2/3 and 5. However, it is unknown whether an analogous inhibitory mechanism separately and specifically controls activity in layer 4. We hypothesized that distinct SST circuits might exist to inhibit specific cortical layers. By enforcing layer-specific inhibition, distinct SST subnetworks could mediate pathway-specific gain control, such as regulating the balance between bottom up and top down input. Employing a combination of high precision circuit mapping, in vivo optogenetic perturbations, and single cell transcriptional profiling, we reveal distinct and complementary SST circuits that specifically and reciprocally interconnect with excitatory cells in either layer 4 or layers 2/3 and 5. Our data further define a transcriptionally distinct SST neuronal sub-class that powerfully gates bottom up sensory activity during active sensation by regulating layer 4 activity. This integrated paradigm further represents a potentially generalizable approach to identify and characterize neuronal cell types and reveal their in vivo function.',
+    'published': '10.7554/eLife.43696',
+    'server': 'biorxiv'}
     
-    # Test a query for a range of dates
-    api.server = "biorxiv"
-    api.interval = "2021/01/01/2021/01/31"
-    response = api.query()
-    assert response.status_code == 200
-
-def test_get_metadata(api):
-    # Test a query for a single manuscript using a DOI
-    api.server = "10.1101/2020.12.31.422412"
-    messages, collections = api.get_metadata()
-    assert len(collections) == 1
-    assert isinstance(collections[0], ManuscriptMetadata)
+def test_query_content_detail_by_doi_matches_example_article_detail(example_article_detail):
+    response = query_content_detail_by_doi(doi=example_article_detail['doi'])
     
-    # Test a query for a range of dates
-    api.server = "biorxiv"
-    api.interval = "2021/01/01/2021/01/31"
-    messages, collections = api.get_metadata()
-    assert len(collections) >= 1
-    assert isinstance(collections[0], ManuscriptMetadata)
+    assert response.status_code == 200
+    assert response.json()['collection'][0] == example_article_detail
+import pytest
+from typing import List
 
-def test_query_many_records(api):
-    # Test a query for many records
-    api.server = "biorxiv"
-    api.interval = "2021/01/01/2021/01/31"
-    collections = api.query_many_records(1000)
-    assert len(collections) == 1000
-    assert isinstance(collections[0], ManuscriptMetadata)
+def test_query_content_detail_by_interval():
+    # Test a valid interval with a cursor value
+    interval = "2018-08-21/2018-08-28"
+    cursor = 45
+    result = query_content_detail_by_interval(interval, cursor=cursor)
+    assert isinstance(result, dict)
+    assert isinstance(result['messages'], list)
+    assert isinstance(result['collections'], list)
+    assert len(result['collections']) >= 0
+        
+    # Test a valid interval without a cursor value
+    interval = "2018-08-21/2018-08-28"
+    result = query_content_detail_by_interval(interval)
+    assert isinstance(result, dict)
+    assert isinstance(result['messages'], list)
+    assert isinstance(result['collections'], list)
+    assert len(result['collections']) >= 0
+        
+    # Test an invalid interval
+    interval = "invalid"
+    with pytest.raises(ValueError):
+        result = query_content_detail_by_interval(interval)
+
+def test_get_all_content_details_by_interval():
+    # Test a valid interval
+    interval = "2018-08-21/2018-08-28"
+    result = get_all_content_details_by_interval(interval)
+    assert isinstance(result, list)
+    assert len(result) >= 0
