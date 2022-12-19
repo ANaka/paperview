a74b703be94fd1bcb8b1058cad2afc945d679c67
anaka <alex.naka@gmail.com>
2022-12-19 10:47:14 -0800

    run precommit config

--- paperview/retrieval/biorxiv.py
+++ paperview/retrieval/biorxiv.py
@@ -1,7 +1,10 @@
-import requests
-from attrs import define, field
 import re
 from typing import List
+
+import requests
+from attrs import define, field
+
+
 @define
 class Message:
     status: str = field()
@@ -10,15 +13,19 @@ class Message:
     count: int = field(default=None)
     count_new_papers: int = field(default=None)
     total: int = field(default=None)
-    
+
     def __repr__(self):
-        return (f"Message(status='{self.status}', interval='{self.interval}', "
-                f"cursor='{self.cursor}', count={self.count}, "
-                f"count_new_papers={self.count_new_papers}, total={self.total})")
+        return (
+            f"Message(status='{self.status}', interval='{self.interval}', "
+            f"cursor='{self.cursor}', count={self.count}, "
+            f"count_new_papers={self.count_new_papers}, total={self.total})"
+        )
+
 
 def split_authors(authors: str) -> list:
     """Split a string of authors into a list of individual author names."""
-    return authors.split('; ')
+    return authors.split("; ")
+
 
 @define
 class ArticleDetail:
@@ -36,7 +43,7 @@ class ArticleDetail:
     published: str = field()
     server: str = field()
     jatsxml: str = field()
-    
+
     def __repr__(self):
         return f"""ArticleDetail(
     title='{self.title}',
@@ -53,41 +60,45 @@ class ArticleDetail:
     published='{self.published}',
     server='{self.server}',
     jatsxml='{self.jatsxml}')"""
-    
+
+
 BASE_URL = "https://api.biorxiv.org"
 
+
 def query_content_detail_by_doi(
-    doi:str, 
-    server:str='biorxiv', # biorxiv or medRxiv
-    format:str='JSON', # JSON or XML
-    ):
-    """https://api.biorxiv.org/details/[server]/[DOI]/na/[format] returns detail for a single manuscript. 
+    doi: str,
+    server: str = "biorxiv",  # biorxiv or medRxiv
+    format: str = "JSON",  # JSON or XML
+):
+    """https://api.biorxiv.org/details/[server]/[DOI]/na/[format] returns detail for a single manuscript.
     For instance, https://api.biorxiv.org/details/biorxiv/10.1101/339747 will output metadata for the biorxiv paper with DOI 10.1101/339747."""
     url = f"{BASE_URL}/details/{server}/{doi}/na/{format}"
     response = requests.get(url)
     return response
 
+
 def validate_interval(interval):
     # Check if the interval is a range of dates
-    if re.match(r'\d{4}-\d{2}-\d{2}/\d{4}-\d{2}-\d{2}', interval):
+    if re.match(r"\d{4}-\d{2}-\d{2}/\d{4}-\d{2}-\d{2}", interval):
         return True
-    
+
     # Check if the interval is a numeric value for the N most recent posts
-    if re.match(r'^\d+$', interval):
+    if re.match(r"^\d+$", interval):
         return True
-    
+
     # Check if the interval is a numeric value with the letter 'd' for the most recent N days of posts
-    if re.match(r'^\d+d$', interval):
+    if re.match(r"^\d+d$", interval):
         return True
-    
+
     return False
+
 
 def query_content_detail_by_interval(
     interval: str,
     cursor: int = 0,
-    server: str='biorxiv', # biorxiv or medRxiv
-    format: str='JSON', # JSON or XML
-    ):
+    server: str = "biorxiv",  # biorxiv or medRxiv
+    format: str = "JSON",  # JSON or XML
+):
     """The format of the endpoint is https://api.biorxiv.org/details/[server]/[interval]/[cursor]/[format] or https://api.biorxiv.org/details/[server]/[DOI]/na/[format]
     where 'interval' can be 1) two YYYY-MM-DD dates separted by '/' and 'cursor' is the start point which defaults to 0 if not supplied, or 2) a numeric value for the N most recent posts, or 3) a numeric with the letter 'd' for the most recent N days of posts.
     Where metadata for multiple papers is returned, results are paginated with 100 papers served in a call. The 'cursor' value can be used to iterate through the result.
@@ -97,23 +108,28 @@ def query_content_detail_by_interval(
     url = f"{BASE_URL}/details/{server}/{interval}/{cursor}/{format}"
     response = requests.get(url)
     # Parse the messages output
-    messages = response.json()['messages']
+    messages = response.json()["messages"]
     parsed_messages = [Message(**message) for message in messages]
-    
-    # Parse the collections output
-    collections = response.json()['collection']
-    parsed_collections = [ArticleDetail(**collection) for collection in collections]
-    
-    # Return the parsed messages and collections
-    return {'messages': parsed_messages, 'collections': parsed_collections}
 
-def get_all_content_details_by_interval(interval: str, server: str='bioRxiv', format: str='json') -> List[ArticleDetail]:
+    # Parse the collections output
+    collections = response.json()["collection"]
+    parsed_collections = [ArticleDetail(**collection) for collection in collections]
+
+    # Return the parsed messages and collections
+    return {"messages": parsed_messages, "collections": parsed_collections}
+
+
+def get_all_content_details_by_interval(
+    interval: str, server: str = "bioRxiv", format: str = "json"
+) -> List[ArticleDetail]:
     """Retrieve all pages of results for the given interval."""
     all_results = []
     cursor = 0
     while True:
-        results = query_content_detail_by_interval(interval, cursor=cursor, server=server, format=format)
-        collections = results['collections']
+        results = query_content_detail_by_interval(
+            interval, cursor=cursor, server=server, format=format
+        )
+        collections = results["collections"]
         all_results.extend(collections)
         if len(collections) < 100:
             # There are no more pages of results
@@ -121,7 +137,8 @@ def get_all_content_details_by_interval(interval: str, server: str='bioRxiv', fo
         cursor += 100
     return all_results
 
-def query_article_by_doi(doi: str, server: str='biorxiv', format: str='JSON'):
+
+def query_article_by_doi(doi: str, server: str = "biorxiv", format: str = "JSON"):
     """https://api.biorxiv.org/pubs/[server]/[DOI]/na/[format] returns detail for a single manuscript.
     For instance, https://api.biorxiv.org/pubs/medrxiv/10.1101/2021.04.29.21256344 will output publication metadata for the biorxiv paper with DOI 10.1101/2021.04.29.21256344. Conversely, https://api.biorxiv.org/pubs/medrxiv/10.1371/journal.pone.0256482 will output publication metadata for the medRxiv paper with published DOI 10.1371/journal.pone.0256482.url = f"{BASE_URL}/details/{server}/{doi}/na/{format}"""
     url = f"{BASE_URL}/pubs/{server}/{doi}/na/{format}"
