992ef385d8ad275c6a41fcc472b6fe2af1966028
anaka <alex.naka@gmail.com>
2022-12-27 12:59:46 -0800

    add modal to fastapi

--- paperview/api/api.py
+++ paperview/api/api.py
@@ -1,8 +1,8 @@
-import requests
-from attrs import asdict
-from bs4 import BeautifulSoup
-from fastapi import FastAPI
+import fastapi
+import modal
+from fastapi.responses import HTMLResponse
 
+from paperview.modal_image import image
 from paperview.retrieval.biorxiv_api import (
     Article,
     ArticleDetail,
@@ -10,17 +10,69 @@ from paperview.retrieval.biorxiv_api import (
     get_content_detail_for_page,
 )
 
-app = FastAPI()
+web_app = fastapi.FastAPI()
+stub = modal.Stub("paperview")
+
+get_overview = modal.lookup("paperview_overview_jobs", "get_overview")
 
 
-@app.get("/")
+@web_app.get("/", response_class=HTMLResponse)
 async def root():
-    return {"message": "Hello World"}
+    html_content = """
+    <html>
+        <head>
+            <title>Paperview</title>
+        </head>
+        <body>
+            <h1>Get a quick look at a bioRxiv manuscript</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
 
 
-@app.get("/metadata/", response_model=ArticleDetail)
+@web_app.get("/metadata/", response_model=ArticleDetail)
 async def get_content_detail(doi: str = None, page: str = None):
     if doi:
         return get_content_detail_by_doi(doi)
     else:
         return get_content_detail_for_page(page)
+
+
+# @web_app.get("/overview/", response_class=HTMLResponse)
+# async def get_overview(doi: str = None, page: str = None):
+#     if doi:
+#         article = Article.from_doi(doi)
+#     else:
+#         article = Article.from_content_page_url(page)
+
+#     overview = article.get_overview(save_images_to_tempfiles=False)
+#     return overview.html
+
+
+@web_app.get("/parse/")
+async def parse(doi: str = None, page: str = None):
+    call = get_overview.spawn(doi=doi, page=page)
+    return {"call_id": call.object_id}
+
+
+@web_app.get("/overview_result/{call_id}", response_class=HTMLResponse)
+async def poll_results(call_id: str):
+    from modal.functions import FunctionCall
+
+    function_call = FunctionCall.from_id(call_id)
+    try:
+        result = function_call.get()
+    except TimeoutError:
+        return fastapi.responses.JSONResponse(content="", status_code=202)
+
+    return result
+
+
+@stub.asgi(image=image)
+def fastapi_app():
+    return web_app
+
+
+if __name__ == "__main__":
+    stub.serve()
